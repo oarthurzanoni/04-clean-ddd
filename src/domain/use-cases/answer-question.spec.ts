3eadf7da0217685ee8fa9d45877b61242317fbf3
Mitacho <arthurflavionz@gmail.com>
2023-07-17 07:38:02 -0300

    Commit: Fundamentos de DDD - Mapeando relacionamentos

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question";
+
+test("create an answer", () => {
+  const answerQuestion = new AnswerQuestionUseCase();
+
+  const answer = answerQuestion.execute({
+    content: "Nova resposta",
+    instructorId: "1",
+    questionId: "1",
+  });
+
+  expect(answer.content).toEqual("Nova resposta");
+  expect(answer).toEqual(
+    expect.objectContaining({
+      id: expect.any(String),
+    })
+  );
+});
