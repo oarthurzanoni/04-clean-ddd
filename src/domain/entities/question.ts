c9040a394db672be8d5d576240de550ae9904642
Mitacho <arthurflavionz@gmail.com>
2023-07-27 07:42:49 -0300

    Fundamentos de DDD - Mapeando propriedades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,13 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
   title: string;
   content: string;
-  authorId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
