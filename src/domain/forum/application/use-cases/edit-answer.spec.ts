e0e07c8cfb5bbffc8e251f5e74f51f0243d1f205
Mitacho <arthurflavionz@gmail.com>
2023-09-27 07:40:50 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Editar resposta

---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,54 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { makeAnswer } from "test/factories/makeAnswer";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { EditAnswerUseCase } from "./edit-answer";
+
+let answersRepository: InMemoryAnswersRepository;
+let sut: EditAnswerUseCase;
+
+describe("Edit Answer", () => {
+  beforeEach(() => {
+    answersRepository = new InMemoryAnswersRepository();
+    sut = new EditAnswerUseCase(answersRepository);
+  });
+
+  it("should be able to edit a answer", async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID("author-1"),
+      },
+      new UniqueEntityID("answer-1")
+    );
+
+    await answersRepository.create(newAnswer);
+
+    await sut.execute({
+      answerId: "answer-1",
+      authorId: "author-1",
+      content: "Conteúdo teste",
+    });
+
+    expect(answersRepository.items[0]).toMatchObject({
+      content: "Conteúdo teste",
+    });
+  });
+
+  it("should not be able to edit a answer from a diffent author", async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID("author-1"),
+      },
+      new UniqueEntityID("answer-1")
+    );
+
+    await answersRepository.create(newAnswer);
+
+    expect(() => {
+      return sut.execute({
+        answerId: "answer-2",
+        authorId: "author-1",
+        content: "Conteúdo teste",
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+});
