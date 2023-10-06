c52f840054d686b5363428062fb905e2eb72aeba
Mitacho <arthurflavionz@gmail.com>
2023-10-06 13:16:26 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Escolher melhor resposta

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -0,0 +1,58 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { makeAnswer } from "test/factories/makeAnswer";
+import { makeQuestion } from "test/factories/makeQuestion";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { ChooseQuestionBestAnswerUseCase } from "./choose-question-best-answer";
+
+let questionsRepository: InMemoryQuestionsRepository;
+let answersRepository: InMemoryAnswersRepository;
+let sut: ChooseQuestionBestAnswerUseCase;
+
+describe("Choose question best answer", () => {
+  beforeEach(() => {
+    questionsRepository = new InMemoryQuestionsRepository();
+    answersRepository = new InMemoryAnswersRepository();
+
+    sut = new ChooseQuestionBestAnswerUseCase(
+      questionsRepository,
+      answersRepository
+    );
+  });
+
+  it("should be able to choose the question best answer", async () => {
+    const question = makeQuestion();
+    const answer = makeAnswer({
+      questionId: question.id,
+    });
+
+    await questionsRepository.create(question);
+    await answersRepository.create(answer);
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: question.authorId.toString(),
+    });
+
+    expect(questionsRepository.items[0].bestAnswerId).toEqual(answer.id);
+  });
+
+  it("should not be able to choose another user question best answer", async () => {
+    const question = makeQuestion({
+      authorId: new UniqueEntityID("author-1"),
+    });
+    const answer = makeAnswer({
+      questionId: question.id,
+    });
+
+    await questionsRepository.create(question);
+    await answersRepository.create(answer);
+
+    expect(() => {
+      return sut.execute({
+        answerId: answer.id.toString(),
+        authorId: "author-2",
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+});
