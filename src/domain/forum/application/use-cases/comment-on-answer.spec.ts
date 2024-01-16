50442e769b038f1bbdd994ec6a9c1e752be14924
Mitacho <arthurflavionz@gmail.com>
2024-01-16 08:55:24 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Comentar na resposta

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,36 @@
+import { makeAnswer } from "test/factories/makeAnswer";
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { CommentOnAnswerUseCase } from "./comment-on-answer";
+
+let answersRepository: InMemoryAnswersRepository;
+let answerCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: CommentOnAnswerUseCase;
+
+describe("Comment on answer", () => {
+  beforeEach(() => {
+    answersRepository = new InMemoryAnswersRepository();
+    answerCommentsRepository = new InMemoryAnswerCommentsRepository();
+
+    sut = new CommentOnAnswerUseCase(
+      answersRepository,
+      answerCommentsRepository
+    );
+  });
+
+  it("should be able to comment on a answer", async () => {
+    const answer = makeAnswer();
+
+    await answersRepository.create(answer);
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: "Comentário teste",
+    });
+
+    expect(answerCommentsRepository.items[0].content).toEqual(
+      "Comentário teste"
+    );
+  });
+});
